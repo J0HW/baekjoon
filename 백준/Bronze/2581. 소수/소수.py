0bbddee66debd42fd4514001c6a6851a020ec554
J0HW <140061065+J0HW@users.noreply.github.com>
2026-03-23 13:11:46 +0900

    [Bronze II] Title: 소수, Time: 588 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.py
@@ -0,0 +1,21 @@
+m=int(input())
+n=int(input())
+arr = []
+
+for i in range(m,n+1):
+    if i < 2:
+        continue
+    if i == 2:
+        arr.append(i)
+        continue
+    for j in range(2, i):
+        if i % j == 0:
+            break
+    else:
+        arr.append(i)
+
+if len(arr) == 0:
+    print(-1)
+else:
+    print(sum(arr))
+    print(arr[0])
